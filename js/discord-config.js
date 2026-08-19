148995e11a3be2ecef6ac0e75cd31ad46ba330d8
NemmarTV <157236315+NemmarTV@users.noreply.github.com>
2026-08-20 01:37:03 +0800

    Add files via upload

---
+++ js/discord-config.js
@@ -0,0 +1,17 @@
+window.PB_DISCORD = {
+  siteUrl: "https://nemmartv.github.io/main1",
+  logoUrl: "https://nemmartv.github.io/main1/images/logo-prime.png",
+  brandLogoUrl: "https://nemmartv.github.io/main1/images/logo-prime.png",
+  // PROH1T applications webhook removed / disabled
+  applicationsWebhook: "",
+  // Announcements to Discord server disabled
+  announcementsWebhook: "",
+  supportWebhook:
+    "https://discordapp.com/api/webhooks/1473273620787630234/RuEApTCwlnNKS6fvmha1LpWoX-rAQgDWD-jZ5XKhCnf2SxOXcemHQIy5SuUJeeIiBvWW",
+  username: "Prime Blog Support",
+  announceUsername: "Prime Blog Announcements",
+  supportUsername: "Prime Blog Support",
+  applicationsMention: "",
+  // Site status notice (used by contact page)
+  siteStatusMessage: "✅ Website is back online! Welcome back to Prime Blog."
+};
